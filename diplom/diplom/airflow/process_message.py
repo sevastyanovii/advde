73ea8f9ee59c59e89f302564278b770d48539319
sevastyanov <ivan.kz@gmail.com>
2022-02-08 23:23:04 +0300

    unzip

--- diplom/diplom/airflow/process_message.py
+++ diplom/diplom/airflow/process_message.py
@@ -1,26 +1,28 @@
-import json
+import boto3
 import ast
+import zipfile
 
 
 def get_file(message):
-    # json_acceptable_string = message.replace("'", "\"")
-    # d = json.loads(json_acceptable_string)
     d = ast.literal_eval(message)
     return {"bucket": d["Records"][0]["s3"]["bucket"]["arn"], "file": d["Records"][0]["s3"]["object"]["key"]}
 
 
 def download_file(file, todir):
-    import boto3
-
-    # s3client = boto3.client("s3", region_name="eu-central-1")
     s3client = boto3.client("s3")
-    # s3client.download_file(file["bucket"], file["file"], todir + "/" + file["file"])
-    s3client.download_file("advde-backet2", file["file"], todir + "/" + file["file"])
+    fn = todir + "/" + file["file"]
+    s3client.download_file("advde-backet2", file["file"], fn)
+    if zipfile.is_zipfile(fn):
+        with zipfile.ZipFile(fn, 'r') as zip_ref:
+            for name in zip_ref.namelist():
+                if name.endswith(".csv") and not name.startswith(".") and name.find("/") < 0:
+                    zip_ref.extract(name, todir)
+                    return True
+        return False
 
 
-# file = {}
 with open("./event_msg.json", "r") as f:
     d = f.read()
     file = get_file(d)
     print(file)
-    download_file(file, "C:\\Users\\vanio\\temp")
+    print(download_file(file, "C:\\Users\\vanio\\temp"))
---
+++ diplom/diplom/airflow/tmp.py
@@ -0,0 +1,3 @@
+import zipfile
+print(zipfile.is_zipfile("C:\\Users\\vanio\\temp\\JC-202201-citibike-tripdata.csv.zip"))
+print(zipfile.is_zipfile("C:\\Users\\vanio\\temp\\1.json"))
